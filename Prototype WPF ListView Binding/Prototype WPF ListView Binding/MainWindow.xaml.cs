f12635ee192ae5bb80a887fbe9355d95ea8dd647
Hans <Hans@DESKTOP-UAD368K>
2019-05-30 12:05:10 +0200

    Projekt "Prototype WPF ListView Binding" erweitert

--- Prototype WPF ListView Binding/Prototype WPF ListView Binding/MainWindow.xaml.cs
+++ Prototype WPF ListView Binding/Prototype WPF ListView Binding/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Data;
+
 namespace Prototype_WPF_ListView_Binding
 {
     /// <summary>
@@ -20,9 +22,62 @@ namespace Prototype_WPF_ListView_Binding
     /// </summary>
     public partial class MainWindow : Window
     {
+        ListViewData listViewData = new ListViewData();
         public MainWindow()
         {
             InitializeComponent();
+
+            GridView gv = new GridView();
+            foreach (DataColumn c in listViewData.dirEntrys.Columns)
+            {
+                GridViewColumn gvColumn = new GridViewColumn();
+                gvColumn.DisplayMemberBinding = new Binding(c.ColumnName);
+                gvColumn.Header = c.ColumnName;
+                gv.Columns.Add(gvColumn);
+            }
+            dirEntryListview.View = gv;
+            dirEntryListview.DataContext = listViewData.dirEntrys;
+            Binding bind = new Binding();
+            dirEntryListview.SetBinding(ListView.ItemsSourceProperty, bind);
+        }
+    }
+    public class ListViewData
+    {
+        private DataTable _dirEntrys;
+        public ListViewData()
+        {
+            createData();
+        }
+        public DataTable dirEntrys
+        {
+            get { return _dirEntrys; }
+        }
+        private void createData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Size");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Type");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("1", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            dt.Rows.Add("1", "", " PRG<");
+            dt.Rows.Add("0", "", " PRG<");
+            _dirEntrys = dt;
         }
     }
 }
